7632e3704540277b88bfca67aaba9ba09ef48c65
Serein <108068915+cafesuada24@users.noreply.github.com>
2025-08-20 13:20:51 +0700

    add launch file for reading camera

--- src/articubot/launch/camera.launch.py
+++ src/articubot/launch/camera.launch.py
@@ -1,17 +1,39 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+
 def generate_launch_description() -> LaunchDescription:
-    return LaunchDescription([
-        Node(
-            package='v4l2_camera',
-            executable='v4l2_camera_node',
-            output='screen',
-            namespace='camera',
-            parameters=[{
-                'image_size': [640,480],
-                'time_per_frame': [1, 6],
-                'camera_frame_id': 'camera_optical_link'
-                }]
+    param_file = os.path.join(
+        get_package_share_directory("articubot"),
+        "config",
+        "cam_params.yaml",
     )
-    ])
+    return LaunchDescription(
+        [
+            # Node(
+            #     package='v4l2_camera',
+            #     executable='v4l2_camera_node',
+            #     output='screen',
+            #     namespace='camera',
+            #     parameters=[{
+            #         'image_size': [1280, 480],
+            #         'time_per_frame': [1, 6],
+            #         'camera_frame_id': 'camera_optical_link'
+            #     }],
+            #     remappings=[("__ns", "/camera")],
+            # ),
+            Node(
+                package="usb_cam",
+                executable="usb_cam_node_exe",
+                arguments=["--ros-args", "--params-file", param_file],
+                remappings=[("__ns", "/camera")],
+            ),
+            Node(
+                package="articubot",
+                executable="usb_cam_preprocessor",
+            ),
+        ]
+    )
