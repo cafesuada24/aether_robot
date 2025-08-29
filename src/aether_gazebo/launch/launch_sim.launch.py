43eab319609f1b49458246a2cfc75d2cb3fbf29f
Serein <108068915+cafesuada24@users.noreply.github.com>
2025-08-29 15:22:41 +0700

    update model

--- src/aether_gazebo/launch/launch_sim.launch.py
+++ src/aether_gazebo/launch/launch_sim.launch.py
@@ -2,8 +2,14 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    ExecuteProcess,
+    IncludeLaunchDescription,
+)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
 from ros_gz_bridge.actions import RosGzBridge
 from ros_gz_sim.actions import GzServer
 
@@ -11,139 +17,83 @@ PKG_NAME: str = 'aether_gazebo'
 
 
 def generate_launch_description() -> LaunchDescription:
+    model_path = LaunchConfiguration('model')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
     pkg_share = get_package_share_directory(PKG_NAME)
-
-    rsp = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [os.path.join(pkg_share, 'launch', 'rsp.launch.py')],
-        ),
-        launch_arguments={'use_sim_time': 'true'}.items(),
-    )
-
-    world_path = os.path.join(pkg_share, 'worlds', 'obstacle.world')
-    gz_server = GzServer(
-        world_sdf_file=world_path,
-        container_name='ros_gz_name',
-        create_own_container='True',
-        use_composition='True',
-    )
-
-    # IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource(
-    #         [
-    #             os.path.join(
-    #                 get_package_share_directory('ros_gz_sim'),
-    #                 'launch',
-    #                 'gz_sim.launch.py',
-    #             ),
-    #         ],
-    #     ),
-    #     launch_arguments={
-    #         'gz_args': ['-r -v2 ', world],
-    #         'on_exit_shutdown': 'true',
-    #     }.items(),
-    # )
-
-    # gzclient_cmd = IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource(
-    #         [
-    #             os.path.join(
-    #                 get_package_share_directory('ros_gz_sim'),
-    #                 'launch',
-    #                 'gz_sim.launch.py',
-    #             ),
-    #         ]
-    #     ),
-    #     launch_arguments={
-    #         'gz_args': ['-g -v2 '],
-    #         'on_exit_shutdown': 'true',
-    #     }.items(),
-    # )
-
     ros_gz_sim_share = get_package_share_directory('ros_gz_sim')
+    default_model_path = os.path.join(pkg_share, 'description', 'robot.urdf.xacro.xml')
+    bridge_config_path = os.path.join(pkg_share, 'config', 'bridge.yaml')
     gz_spawn_model_launch_source = os.path.join(
         ros_gz_sim_share,
         'launch',
         'gz_spawn_model.launch.py',
     )
+    world_path = os.path.join(pkg_share, 'worlds', 'obstacle.world')
+
+
+    robot_description_config = Command(
+        [
+            'xacro ',
+            model_path,
+            ' sim_mode:=',
+            use_sim_time,
+        ],
+    )
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[
+            {
+                'robot_description': robot_description_config,
+                'use_sim_time': use_sim_time,
+            },
+        ],
+    )
+
+    gz_server = GzServer(
+        world_sdf_file=world_path,
+        container_name='ros_gz_container',
+        create_own_container='True',
+        use_composition='True',
+    )
+
+    gz_client_cmd = ExecuteProcess(cmd=['gz', 'sim', '-g', '-r'], output='screen')
+
     spawn_entity = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(gz_spawn_model_launch_source),
         launch_arguments={
             'world': 'default',
             'topic': '/robot_description',
-            'entity_name': 'articubot',
+            'entity_name': 'aether_bot',
+            'z': '0.175',
         }.items(),
-        # package='ros_gz_sim',
-        # executable='create',
-        # arguments=[
-        #     '-topic',
-        #     'robot_description',
-        #     '-entity',
-        #     'my_bot',
-        #     '-x',
-        #     '0.0',
-        #     '-y',
-        #     '0.0',
-        #     '-z',
-        #     '2',
-        # ],
-        # output='screen',
     )
 
-    bridge_params = os.path.join(pkg_share, 'config', 'bridge.yaml')
-    bridge_cmd = RosGzBridge(
+    ros_gz_bridge = RosGzBridge(
         bridge_name='ros_gz_bridge',
-        config_file=bridge_params,
+        config_file=bridge_config_path,
         container_name='ros_gz_container',
         create_own_container='False',
         use_composition='True',
     )
 
-    # Node(
-    #     package='ros_gz_bridge',
-    #     executable='parameter_bridge',
-    #     arguments=[
-    #         '--ros-args',
-    #         '-p',
-    #         f'config_file:={bridge_params}',
-    #     ],
-    # )
-
-    # robot_controller_spawner = Node(
-    #     package='controller_manager',
-    #     executable='spawner',
-    #     # arguments=['four_wheel_controller', '--param-file', robot_controllers],
-    #     arguments=['four_wheel_controller'],
-    # )
-    # joint_state_broadcaster_spawner = Node(
-    #     package='controller_manager',
-    #     executable='spawner',
-    #     arguments=['joint_state_broadcaster'],
-    # )
-    #
-    # delay_joint_state_broadcaster_after_robot_controller_spawner = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=spawn_entity,
-    #         on_exit=[joint_state_broadcaster_spawner],
-    #     ),
-    # )
-    #
-    # delay_controller_spawner_after_joint_state_broadcaster = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=joint_state_broadcaster_spawner,
-    #         on_exit=[robot_controller_spawner],
-    #     ),
-    # )
-
     return LaunchDescription(
         [
-            # gzserver_cmd,
-            # gzclient_cmd,
+            DeclareLaunchArgument(
+                name='model',
+                default_value=default_model_path,
+                description='Absolute path to robot model file',
+            ),
+            DeclareLaunchArgument(
+                name='use_sim_time',
+                default_value='True',
+                description='Flag to enable use_sim_time',
+            ),
             gz_server,
-            rsp,
-            bridge_cmd,
+            gz_client_cmd,
+            robot_state_publisher_node,
+            ros_gz_bridge,
             spawn_entity,
-            # delay_joint_state_broadcaster_after_robot_controller_spawner,
-            # delay_controller_spawner_after_joint_state_broadcaster,
         ],
     )
