a4bbfc8d5cb07802592dc19bbf23f3e9987acdbd
Serein <108068915+cafesuada24@users.noreply.github.com>
2025-09-01 17:03:31 +0700

    update source script

--- scripts/source_env.zsh
+++ scripts/source_env.zsh
@@ -2,6 +2,7 @@ ROS_DISTRO=jazzy
 
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export GZ_SIM_SYSTEM_PLUGIN_PATH=/opt/ros/${ROS_DISTRO}/lib/
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/opt/ros/jazzy/share/turtlebot3_gazebo/models
 
 echo "Sourcing ROS $ROS_DISTRO underlay environment..."
 source /opt/ros/$ROS_DISTRO/setup.zsh && echo "Done."
