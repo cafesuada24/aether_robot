4538ca4d95d8c77a2369dfe3df3b45b72faa394c
Serein <108068915+cafesuada24@users.noreply.github.com>
2025-09-16 21:51:24 +0700

    rename

--- scripts/setup.zsh
+++ scripts/setup.zsh
@@ -1,11 +1,11 @@
-ROS_DISTRO=jazzy
+ROS_DISTRO='jazzy'
 
-export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-export GZ_SIM_SYSTEM_PLUGIN_PATH=/opt/ros/${ROS_DISTRO}/lib/
-export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/opt/ros/jazzy/share/turtlebot3_gazebo/models
+export RMW_IMPLEMENTATION='rmw_cyclonedds_cpp'
+export GZ_SIM_SYSTEM_PLUGIN_PATH="/opt/ros/${ROS_DISTRO}/lib/"
+export GAZEBO_MODEL_PATH="$GAZEBO_MODEL_PATH:/opt/ros/jazzy/share/turtlebot3_gazebo/models"
 
 echo "Sourcing ROS $ROS_DISTRO underlay environment..."
-source /opt/ros/$ROS_DISTRO/setup.zsh && echo 'Done.'
+source "/opt/ros/$ROS_DISTRO/setup.zsh" && echo 'Done.'
 
 echo 'Sourcing workspace overlay environment...'
 source ./install/setup.zsh && echo "Done."
