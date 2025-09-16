c712dccf3d540f68c697c34097b35e6151733c20
Serein <108068915+cafesuada24@users.noreply.github.com>
2025-09-16 15:52:43 +0700

    add venv activating commands

--- scripts/source_env.zsh
+++ scripts/source_env.zsh
@@ -5,10 +5,15 @@ export GZ_SIM_SYSTEM_PLUGIN_PATH=/opt/ros/${ROS_DISTRO}/lib/
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/opt/ros/jazzy/share/turtlebot3_gazebo/models
 
 echo "Sourcing ROS $ROS_DISTRO underlay environment..."
-source /opt/ros/$ROS_DISTRO/setup.zsh && echo "Done."
+source /opt/ros/$ROS_DISTRO/setup.zsh && echo 'Done.'
 
-echo "Sourcing workspace overlay environment..."
+echo 'Sourcing workspace overlay environment...'
 source ./install/setup.zsh && echo "Done."
 
 eval "$(register-python-argcomplete ros2)"
 eval "$(register-python-argcomplete colcon)"
+
+echo 'Activating python environment...'
+source ./venv/bin/activate
+touch ./venv/COLCON_IGNORE
+echo 'Done.'
