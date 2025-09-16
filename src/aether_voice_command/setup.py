77b83a1d0744506e57692e33b17af2b25f938ee0
Serein <108068915+cafesuada24@users.noreply.github.com>
2025-09-16 11:23:15 +0700

    allow launch directory

--- src/aether_voice_command/setup.py
+++ src/aether_voice_command/setup.py
@@ -1,3 +1,6 @@
+import os
+from glob import glob
+
 from setuptools import find_packages, setup
 
 package_name = 'aether_voice_command'
@@ -7,19 +10,18 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='serein',
+    maintainer='cafesuada',
     maintainer_email='108068915+cafesuada24@users.noreply.github.com',
     description='TODO: Package description',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-        ],
+        'console_scripts': [],
     },
 )
