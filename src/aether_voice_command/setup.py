7d3a25ad869d237fc6f2f0fd53707b4dbda7793e
Serein <108068915+cafesuada24@users.noreply.github.com>
2025-09-18 17:05:07 +0700

    update python setup script

--- src/aether_voice_command/setup.py
+++ src/aether_voice_command/setup.py
@@ -14,7 +14,16 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*')),
     ],
-    install_requires=['setuptools'],
+    install_requires=[
+        'setuptools',
+        'semantic-kernel[google]',
+        'dotenv',
+        'typeguard',
+        'dotenv',
+        'catkin-pkg',
+        'empy',
+        'lark',
+    ],
     zip_safe=True,
     maintainer='cafesuada',
     maintainer_email='108068915+cafesuada24@users.noreply.github.com',
@@ -22,6 +31,9 @@ setup(
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [],
+        'console_scripts': [
+            f'llm_service = {package_name}.service:main',
+            f'llm_client = {package_name}.client:main',
+        ],
     },
 )
