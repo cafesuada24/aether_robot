fd67db22cbf561b3c4a73b4d307edd27aa1d827f
Serein <108068915+cafesuada24@users.noreply.github.com>
2025-09-26 13:24:51 +0700

    update launch file

--- src/aether/launch/aether.launch.py
+++ src/aether/launch/aether.launch.py
@@ -2,6 +2,12 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+)
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 PKG_NAME = 'aether'
@@ -9,25 +15,58 @@ PKG_NAME = 'aether'
 
 def generate_launch_description() -> LaunchDescription:
     """Generate launch description for aether."""
-    pkg_share = get_package_share_directory(PKG_NAME)
+    cmd_vel_out_topic = LaunchConfiguration('cmd_vel_out_topic')
+    sim_mode = LaunchConfiguration('sim_mode')
 
+    pkg_share = get_package_share_directory(PKG_NAME)
+    aether_nav_share = get_package_share_directory('aether_navigation')
     twist_mux_params_file = os.path.join(
         pkg_share,
         'params',
         'twist_mux.yaml',
     )
+
     twist_mux_node = Node(
         package='twist_mux',
         executable='twist_mux',
         output='screen',
-        remappings=[('/cmd_vel_out', 'cmd_vel')],
+        remappings=[('/cmd_vel_out', cmd_vel_out_topic)],
         parameters=[twist_mux_params_file],
     )
 
-    twist_stamper_node = Node(
-        package='aether',
-        executable='twist_stamper_node',
-        output='screen',
+    slam_online_async_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(aether_nav_share, 'launch', 'slam_online_async_launch.py'),
+        ),
+        launch_arguments={
+            'use_sim_time': sim_mode,
+        }.items(),
     )
 
-    return LaunchDescription([twist_mux_node])
+    navigation_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(aether_nav_share, 'launch', 'navigation_launch.py'),
+        ),
+        launch_arguments={
+            'use_sim_time': sim_mode,
+        }.items(),
+    )
+
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                name='cmd_vel_out_topic',
+                default_value='cmd_vel',
+                description='Topic that receives twist data',
+            ),
+            DeclareLaunchArgument(
+                name='sim_mode',
+                default_value='false',
+                description='Flag to enable simulation mode',
+                choices=['true', 'false'],
+            ),
+            twist_mux_node,
+            slam_online_async_launch,
+            navigation_launch,
+        ],
+    )
