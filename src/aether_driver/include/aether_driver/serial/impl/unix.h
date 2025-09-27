4fe9b91f31084e3eef5eef4a8641db5d8d7ec744
Serein <108068915+cafesuada24@users.noreply.github.com>
2025-09-27 14:45:53 +0700

    add serial lib

---
+++ src/aether_driver/include/aether_driver/serial/impl/unix.h
@@ -0,0 +1,142 @@
+#if !defined(_WIN32)
+
+#ifndef AETHER_DRIVER__IMPL__UNIX_H_
+#define AETHER_DRIVER__IMPL__UNIX_H_
+
+#include <pthread.h>
+
+#include "aether_driver/serial/serial.h"
+
+namespace aether_driver {
+
+using std::invalid_argument;
+using std::size_t;
+using std::string;
+
+using aether_driver::IOException;
+using aether_driver::SerialException;
+
+class MillisecondTimer {
+ public:
+  explicit MillisecondTimer(const uint32_t millis);
+  int64_t remaining();
+
+ private:
+  static timespec timespec_now();
+  timespec expiry;
+};
+
+class aether_driver::Serial::SerialImpl {
+ public:
+  SerialImpl(const string &port, uint64_t baudrate, bytesize_t bytesize,
+             parity_t parity, stopbits_t stopbits, flowcontrol_t flowcontrol);
+
+  virtual ~SerialImpl();
+
+  void open();
+
+  void close();
+
+  bool isOpen() const;
+
+  size_t available();
+
+  bool waitReadable(uint32_t timeout);
+
+  void waitByteTimes(size_t count);
+
+  size_t read(uint8_t *buf, size_t size = 1);
+
+  size_t write(const uint8_t *data, size_t length);
+
+  void flush();
+
+  void flushInput();
+
+  void flushOutput();
+
+  void sendBreak(int duration);
+
+  void setBreak(bool level);
+
+  void setRTS(bool level);
+
+  void setDTR(bool level);
+
+  bool waitForChange();
+
+  bool getCTS();
+
+  bool getDSR();
+
+  bool getRI();
+
+  bool getCD();
+
+  void setPort(const string &port);
+
+  string getPort() const;
+
+  void setTimeout(Timeout &timeout);
+
+  Timeout getTimeout() const;
+
+  void setBaudrate(uint64_t baudrate);
+
+  uint64_t getBaudrate() const;
+
+  void setBytesize(bytesize_t bytesize);
+
+  bytesize_t getBytesize() const;
+
+  void setParity(parity_t parity);
+
+  parity_t getParity() const;
+
+  void setStopbits(stopbits_t stopbits);
+
+  stopbits_t getStopbits() const;
+
+  void setFlowcontrol(flowcontrol_t flowcontrol);
+
+  flowcontrol_t getFlowcontrol() const;
+
+  void readLock();
+
+  void readUnlock();
+
+  void writeLock();
+
+  void writeUnlock();
+
+ protected:
+  void reconfigurePort();
+
+ private:
+  string port_;  // Path to the file descriptor
+  int fd_;       // The current file descriptor
+
+  bool is_open_;
+  bool xonxoff_;
+  bool rtscts_;
+
+  Timeout timeout_;         // Timeout for read operations
+  uint64_t baudrate_;  // Baudrate
+  uint32_t byte_time_ns_;   // Nanoseconds to transmit/receive a single byte
+
+  parity_t parity_;            // Parity
+  bytesize_t bytesize_;        // Size of the bytes
+  stopbits_t stopbits_;        // Stop Bits
+  flowcontrol_t flowcontrol_;  // Flow Control
+
+  // Mutex used to lock the read functions
+  pthread_mutex_t read_mutex;
+  // Mutex used to lock the write functions
+  pthread_mutex_t write_mutex;
+};
+
+}  // namespace aether_driver
+
+#endif  // AETHER_DRIVER__IMPL__UNIX_H_
+
+#endif  // !defined(_WIN32)
