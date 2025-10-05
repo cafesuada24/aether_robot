613e298f9e28987bdaf54baeb48f8d5e1329aa8a
Serein <108068915+cafesuada24@users.noreply.github.com>
2025-10-05 16:21:01 +0700

    implement constructor and destructor

--- src/aether_webserver/src/service_advertiser.cpp
+++ src/aether_webserver/src/service_advertiser.cpp
@@ -11,9 +11,29 @@
 class ServiceAdvertiser : public rclcpp::Node {
  public:
   ServiceAdvertiser() : Node("service_advertiser") {
+    if (!(simple_poll_ = avahi_simple_poll_new())) {
+      RCLCPP_ERROR(get_logger(), "Failed to create simple poll object.");
+      throw std::exception();
+    }
+    client_ =
+        avahi_client_new(avahi_simple_poll_get(simple_poll_),
+                         AVAHI_CLIENT_NO_FAIL, client_callback, this, &error_);
+    if (!client_) {
+      RCLCPP_ERROR(get_logger(), "Failed to create client: %s.",
+                   avahi_strerror(error_));
+      throw std::exception();
+    }
   }
 
   ~ServiceAdvertiser() {
+    avahi_simple_poll_quit(simple_poll_);
+    if (client_) {
+      avahi_client_free(client_);
+    }
+    if (simple_poll_) {
+      avahi_simple_poll_free(simple_poll_);
+    }
+    RCLCPP_INFO(this->get_logger(), "Avahi service unregistered.");
   }
 
  private:
