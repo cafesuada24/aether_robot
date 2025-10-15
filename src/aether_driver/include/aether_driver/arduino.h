3426a1b560cebe61f7305707eb810dde817ca98b
Serein <108068915+cafesuada24@users.noreply.github.com>
2025-10-15 08:54:00 +0700

    impl: read encoder values and set PID params

--- src/aether_driver/include/aether_driver/arduino.h
+++ src/aether_driver/include/aether_driver/arduino.h
@@ -3,6 +3,7 @@
 
 #include <cstdint>
 #include <string>
+
 #include "aether_driver/serial/serial.h"
 namespace aether_driver {
 class Arduino {
@@ -11,32 +12,38 @@ class Arduino {
                    const uint16_t braud = 9600,
                    const uint64_t timeout_ms = 1000,
                    const uint16_t encoder_resolution = 20,
-                   const float wheel_diameter_meter = 0.01, 
+                   const float wheel_diameter_meter = 0.01,
                    // const float gear_reduction = 1/120,
                    const float min_linear_speed_m_per_s = 1.0,
-                   const float max_linear_speed_m_per_s = 0.0
-                   );
+                   const float max_linear_speed_m_per_s = 0.0);
 
   bool drive_m_per_sec(const float left_speed, const float right_speed);
 
   bool stop_motors();
+
  private:
-  static constexpr auto PID_RATE {30};
-  static constexpr auto PID_INTERVAL {1000.0 / 30};
+  static constexpr auto PID_RATE{30};
+  static constexpr auto PID_INTERVAL{1000.0 / 30};
 
-  Serial serial_ {};
-  
-  uint16_t encoder_count_ {0};
-  uint16_t encoder_resolution_ {};
-  float wheel_diameter_meter_ {}; 
+  Serial serial_{};
+
+  uint16_t encoder_count_{0};
+  uint16_t encoder_resolution_{};
+  float wheel_diameter_meter_{};
   // float gear_reduction_ {};
-  float min_linear_speed_m_per_s_ {};
-  float max_linear_speed_m_per_s_ {};
+  float min_linear_speed_m_per_s_{};
+  float max_linear_speed_m_per_s_{};
 
-  bool drive(const uint16_t left_ticks_per_loop, const uint16_t right_ticks_per_loop);
+  bool drive(const uint16_t left_ticks_per_loop,
+             const uint16_t right_ticks_per_loop);
+  bool set_pid_values(const float k_p, const float k_d, const float k_i,
+                      const float k_o);
+  void read_encoder_values(uint64_t& left, uint64_t& right);
 
   int16_t calc_ticks_per_loop(const float speed) const;
   float ensure_speed(const float speed) const;
+
+  bool connected() const;
 };
 }  // namespace aether_driver
 
--- src/aether_driver/src/lib/arduino.cc
+++ src/aether_driver/src/lib/arduino.cc
@@ -2,9 +2,13 @@
 
 #include <algorithm>
 #include <cmath>
+#include <exception>
 #include <iostream>
+#include <string>
 
 namespace aether_driver {
+
+using namespace std::string_literals;
 Arduino::Arduino(const std::string& serial, const uint16_t braud,
                  const uint64_t timeout_ms, const uint16_t encoder_resolution,
                  const float wheel_diameter_meter,
@@ -32,21 +36,25 @@ Arduino::Arduino(const std::string& serial, const uint16_t braud,
 int16_t Arduino::calc_ticks_per_loop(const float speed) const {
   if (wheel_diameter_meter_ <= 0) {
     std::cerr << "wheel diameter is not set";
-    throw aether_driver::SerialException("Parameter 'wheel_diameter' is not set correctly.");
+    throw aether_driver::SerialException(
+        "Parameter 'wheel_diameter' is not set correctly.");
   }
 
   if (encoder_resolution_ <= 0) {
     std::cerr << "encoder resolution is not set";
-    throw aether_driver::SerialException("Parameter 'encoder_resolution' is not set correctly.");
+    throw aether_driver::SerialException(
+        "Parameter 'encoder_resolution' is not set correctly.");
   }
 
-  return std::round(speed / (wheel_diameter_meter_ * M_PI * PID_RATE) * encoder_resolution_);
+  return std::round(speed / (wheel_diameter_meter_ * M_PI * PID_RATE) *
+                    encoder_resolution_);
 }
 
 float Arduino::ensure_speed(const float speed) const {
-  float spd {0.0};
+  float spd{0.0};
   if (speed != 0.0) {
-    spd = std::clamp((speed < 0 ? -1 : 1) * speed, min_linear_speed_m_per_s_, max_linear_speed_m_per_s_);
+    spd = std::clamp((speed < 0 ? -1 : 1) * speed, min_linear_speed_m_per_s_,
+                     max_linear_speed_m_per_s_);
     if (speed < 0) {
       spd *= -1;
     }
@@ -55,8 +63,8 @@ float Arduino::ensure_speed(const float speed) const {
 };
 
 bool Arduino::drive_m_per_sec(const float left_speed, const float right_speed) {
-  const auto left_spd {ensure_speed(left_speed)};
-  const auto right_spd {ensure_speed(right_speed)};
+  const auto left_spd{ensure_speed(left_speed)};
+  const auto right_spd{ensure_speed(right_speed)};
 
   // std::ostringstream oss{};
   //
@@ -73,7 +81,6 @@ bool Arduino::drive_m_per_sec(const float left_speed, const float right_speed) {
   // serial_.write(oss.str());
   //
   // return std::strcmp(serial_.read(2).c_str(), "OK") == 0;
-
 
   // if (gear_reduction_ <= 0) {
   //   std::cerr << "gear reduction is not set";
@@ -110,6 +117,30 @@ bool Arduino::drive(const uint16_t left_ticks_per_loop,
 
   serial_.write(oss.str());
 
+  return std::strcmp(serial_.read(2).c_str(), "OK") == 0;
+}
+
+bool Arduino::connected() const { return serial_.isOpen(); }
+
+void Arduino::read_encoder_values(uint64_t& left, uint64_t& right) {
+  serial_.write("e\r"s);
+
+  std::string response {};
+  serial_.readline(response);
+
+  const std::string delimiter { " " };
+  size_t del_pos {response.find(delimiter) };
+  std::string token_1 {response.substr(0, del_pos) };
+  std::string token_2 {response.substr(del_pos + delimiter.length())};
+
+  left = std::atoll(token_1.c_str());
+  right = std::atoll(token_2.c_str());
+};
+
+bool Arduino::set_pid_values(const float k_p, const float k_d, const float k_i, const float k_o) {
+  std::stringstream ss;
+  ss << "u " << k_p << ":" << k_d << ":" << k_i << ":" << k_o << "\r";
+  serial_.write(ss.str());
   return std::strcmp(serial_.read(2).c_str(), "OK") == 0;
 }
 }  // namespace aether_driver
