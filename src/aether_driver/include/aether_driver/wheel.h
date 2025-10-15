bc864d6c21c166ad4e1674e7a1c5a37dacc23de3
Serein <108068915+cafesuada24@users.noreply.github.com>
2025-10-15 10:36:16 +0700

    update declaration

--- src/aether_driver/include/aether_driver/wheel.h
+++ src/aether_driver/include/aether_driver/wheel.h
@@ -6,26 +6,25 @@
 
 namespace aether_driver {
 
-class Wheel
-{
-    public:
+class Wheel {
+ public:
+  std::string name{""};
+  uint64_t enc{0};
+  double cmd{0};
+  double pos{0};
+  double vel{0};
+  double eff{0};
+  double velSetPt{0};
+  double rads_per_count{0};
 
-    std::string name {""};
-    uint64_t enc { 0 };
-    double cmd { 0 };
-    double pos { 0 };
-    double vel { 0 };
-    double eff { 0 };
-    double velSetPt { 0 };
-    double rads_per_count { 0 };
+  Wheel() = default;
 
-    Wheel() = default;
+  Wheel(const std::string &wheel_name, const int counts_per_rev);
 
-    Wheel(const std::string &wheel_name, const int counts_per_rev);
-    
-    double calcEncAngle() const;
+  void setup(const std::string &wheel_name, const int counts_per_rev);
+
+  double calc_enc_angle() const;
 };
 }  // namespace aether_driver
 
-
-#endif // AETHER_DRIVER__WHEEL_H_
+#endif  // AETHER_DRIVER__WHEEL_H_
