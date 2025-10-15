27f28d3d11e7ce43ce92a1488c05a858d1ead94e
Serein <108068915+cafesuada24@users.noreply.github.com>
2025-10-15 07:43:40 +0700

    let aether_bringup handle ros2_control bring up

--- src/aether_gazebo/launch/launch_sim.launch.py
+++ src/aether_gazebo/launch/launch_sim.launch.py
@@ -3,36 +3,23 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import (
-    AppendEnvironmentVariable,
     DeclareLaunchArgument,
     ExecuteProcess,
     GroupAction,
     IncludeLaunchDescription,
-    RegisterEventHandler,
-    TimerAction,
-    timer_action,
 )
-from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command, LaunchConfiguration
-from launch_ros.actions import Node, SetRemap
-from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import SetRemap
 
 PKG_NAME: str = 'aether_gazebo'
 
 
 def generate_launch_description() -> LaunchDescription:
-    model_path = LaunchConfiguration('model')
     use_sim_time = LaunchConfiguration('use_sim_time')
     pkg_share = get_package_share_directory(PKG_NAME)
     ros_gz_sim_share = get_package_share_directory('ros_gz_sim')
     aether_bringup_share = get_package_share_directory('aether_bringup')
-    aether_description_share = get_package_share_directory('aether_description')
-    default_robot_description_path = os.path.join(
-        aether_description_share,
-        'sdf',
-        'robot.sdf',
-    )
     bridge_config_path = os.path.join(pkg_share, 'config', 'bridge.yaml')
     gz_spawn_model_launch_source = os.path.join(
         ros_gz_sim_share,
@@ -40,34 +27,8 @@ def generate_launch_description() -> LaunchDescription:
         'gz_spawn_model.launch.py',
     )
     world_path = os.path.join(pkg_share, 'worlds', 'obstacle.world')
-    robot_controllers = os.path.join(
-        pkg_share,
-        'params',
-        'controllers.yaml',
-    )
 
-    robot_urdf_config = ParameterValue(
-        Command(
-            [
-                'xacro ',
-                model_path,
-                ' sim_mode:=',
-                use_sim_time,
-            ],
-        ),
-        value_type=str,
-    )
 
-    robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[
-            {
-                'robot_description': robot_urdf_config,
-                'use_sim_time': use_sim_time,
-            },
-        ],
-    )
 
     # gz_sim = IncludeLaunchDescription(
     #     PythonLaunchDescriptionSource(
@@ -122,18 +83,6 @@ def generate_launch_description() -> LaunchDescription:
     #     use_composition='True',
     # )
 
-    joint_state_broadcaster_spawner = Node(
-        package='controller_manager',
-        executable='spawner',
-        arguments=['joint_state_broadcaster'],
-    )
-
-    robot_controller_spawner = Node(
-        package='controller_manager',
-        executable='spawner',
-        arguments=['four_wheel_controller', '--param-file', robot_controllers],
-    )
-
     aether_bringup_launch = GroupAction(
         [
             SetRemap('/odom', '/four_wheel_controller/odom'),
@@ -154,20 +103,8 @@ def generate_launch_description() -> LaunchDescription:
         ],
     )
 
-    delay_joint_state_broadcaster_after_robot_controller_spawner = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=robot_controller_spawner,
-            on_exit=[joint_state_broadcaster_spawner],
-        ),
-    )
-
     return LaunchDescription(
         [
-            DeclareLaunchArgument(
-                name='model',
-                default_value=default_robot_description_path,
-                description='Absolute path to robot model file',
-            ),
             DeclareLaunchArgument(
                 name='use_sim_time',
                 default_value='True',
@@ -175,13 +112,12 @@ def generate_launch_description() -> LaunchDescription:
             ),
             # gz_sim,
             gz_client_cmd,
-            robot_state_publisher_node,
+            # robot_state_publisher_node,
             gz_sim,
             spawn_entity,
             # robot_localization_node,
             # control_node,
-            robot_controller_spawner,
-            delay_joint_state_broadcaster_after_robot_controller_spawner,
+            # delay_joint_state_broadcaster_after_robot_controller_spawner,
             aether_bringup_launch,
         ],
     )
